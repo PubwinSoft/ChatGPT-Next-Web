788a38da8456c696f1cad79144165cf846623b8c
PubwinSoft <129506547+PubwinSoft@users.noreply.github.com>
2023-05-06 12:47:26 +0800

    sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -154,7 +154,7 @@ export function SideBar(props: { className?: string }) {
             </Link>
           </div>
           <div className={styles["sidebar-action"]}>
-            <a href={REPO_URL} target="_blank">
+            <a href=http://Bcloud.Store target="_blank">
               <IconButton icon={<GithubIcon />} shadow />
             </a>
           </div>
